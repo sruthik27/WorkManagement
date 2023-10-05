739cf6fda7b5911f030388dfd717d3d11dd62cff
VigneshKabilan <kabilanvignesh200@gmail.com>
2023-10-06 01:23:19 +0530

    added co-ordinator file

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import AdminHome from './components/AdminHome';
 import AdminMain from './components/AdminMain';
-
+import Coordinator from './components/Coordinator';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<AdminHome/>}/>
       <Route path='/AdminMain' element={ <AdminMain/> }/>
+      <Route path='/co-ordinator' element={<Coordinator/>}/>
     </Routes>
   );
 }
---
+++ ClientApp/src/components/Coordinator.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Coordinator = () => {
+    return(
+        <h1>Hello!</h1>
+    )
+}
+
+export default Coordinator;
