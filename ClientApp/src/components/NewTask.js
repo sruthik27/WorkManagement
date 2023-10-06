4aa5101099b6d28afcace9a1da23cf66d3e4b3ed
VigneshKabilan <kabilanvignesh200@gmail.com>
2023-10-06 23:03:11 +0530

    added new taske editor

--- ClientApp/src/components/NewTask.js
+++ ClientApp/src/components/NewTask.js
@@ -1,8 +1,27 @@
 import React from "react";
+import "./NewTask.css";
 
 const NewTask = () => {
     return(
-        <h1>This is NewTask</h1>
+        <>
+            <div className="Home">
+                <h1 className="home-title">PUBLISH NEW TASK</h1>
+                <div className="input-div-1">
+                    <input className="input-bar-1" type="text" placeholder="Type Work Name" />
+                    <input className="input-bar-1" type="text" placeholder="Type Work Cost" />
+                </div>
+                <div className="input-div-1">
+                    <input className="input-bar-1" type="text" placeholder="Start Date" />
+                    <input className="input-bar-1" type="text" placeholder="Due Date" />
+                </div>
+                <div className="input-div-2">
+                    <input className="input-bar-2" type="text" placeholder="Type task description" />
+                    <input className="input-bar-2" type="text" placeholder="Type Date of Advance payment" />
+                    <input className="input-bar-2" type="text" placeholder="Type amount paid in advance " />
+                </div>
+                <button className="Add-button">Add Task</button>
+            </div>
+        </>
     )
 }
 
